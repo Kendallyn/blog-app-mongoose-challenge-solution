681a4a535a67e9cb500398a6b8c1d4107c3f8ada
Kendallyn <Kendallynlyon@hotmail.com>
2017-05-15 08:30:11 -0700

    database url changed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+    'mongodb: //admin:password@ds137801.mlab.com:37801/test-blog-integration';
 exports.PORT = process.env.PORT || 8080;
